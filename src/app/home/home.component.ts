e2dfbc255ad8e40ecf64ba1feea675c7bc196b69
Shailendra Bhargava <shailendrabhargavaa@gmail.com>
2024-10-28 17:38:20 +0530

    updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -47,7 +47,7 @@ export class HomeComponent {
   ];
 
   constructor(private app: AppService, private message: NzMessageService) {
-    this.getGreeting();
+    //this.getGreeting();
     this.app.showSpinner();
     this.app.userEmail
       .pipe(
@@ -90,23 +90,23 @@ export class HomeComponent {
       .subscribe();
   }
 
-  getGreeting() {
-    var data: any = [
-        [22, 'Working late'],
-        [18, 'Good evening'],
-        [12, 'Good afternoon'],
-        [5, 'Good morning'],
-        [0, 'Whoa, early bird'],
-      ],
-      hr = new Date().getHours();
-    for (var i = 0; i < data.length; i++) {
-      if (hr >= data[i][0]) {
-        const text = data[i][1];
-        this.greeting = text;
-        break;
-      }
-    }
-  }
+  // getGreeting() {
+  //   var data: any = [
+  //       [22, 'Working late'],
+  //       [18, 'Good evening'],
+  //       [12, 'Good afternoon'],
+  //       [5, 'Good morning'],
+  //       [0, 'Whoa, early bird'],
+  //     ],
+  //     hr = new Date().getHours();
+  //   for (var i = 0; i < data.length; i++) {
+  //     if (hr >= data[i][0]) {
+  //       const text = data[i][1];
+  //       this.greeting = text;
+  //       break;
+  //     }
+  //   }
+  // }
 
   getTodaysTotalSpending(transactions: any[]): number {
     const today = new Date().toISOString().split('T')[0];
